Fixed gradient

AeViz/utils/math_utils.py
@@ -151,9 +151,9 @@ def gradient(quantity, x, y, z,
     else:
         dr = IDL_derivative(x, quantity, 'radius')[None, ...]
         dth = (1 / x[None, None, :] * 
-               IDL_derivative(x, quantity, 'theta'))[None, ...]
+               IDL_derivative(y, quantity, 'theta'))[None, ...]
         dph = (1 / (x[None, None, :] * np.sin(y[None, :, None])) *
-               IDL_derivative(x, quantity, 'theta'))[None, ...]
+               IDL_derivative(z, quantity, 'theta'))[None, ...]
         return np.concatenate((dr, dth, dph), axis=0)
 
 def get_stream_quantities(b1, b2, ax, ay, az, lx, ly, lz, plane):