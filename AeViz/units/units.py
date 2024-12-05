49b912e5294d2177ea147bfb33177d15b9e1b12e
MarcoCusinato <marco.cusinato@uv.es>
2024-12-05 18:47:13 +0100

    Added CActus conversion

--- AeViz/units/units.py
+++ AeViz/units/units.py
@@ -9,6 +9,8 @@ class units:
         self.km = 1e5 #cm
         self.ms = 1e-3 #s
         self.G = 6.67e-8#cm^3/(g*s^2)
+        self.L_cactus = self.G * self.solar_mass / self.speed_light ** 2
+        self.T_cactus = self.G * self.solar_mass / self.speed_light ** 3
 
     def convert_to_solar_masses(self, quantity):
         return quantity / self.solar_mass
@@ -43,3 +45,36 @@ class units:
     def convert_to_grav_mass(self, mass, energy):
         return mass + self.convert_to_solar_masses(
             energy / self.speed_light ** 2)
+    
+    ## Cactus units
+    ## c=G=M_sun=1
+    
+    def convert_to_cactus_lenght(self, quantity):
+        return quantity / self.L_cactus
+    
+    def convert_from_cactus_lenght(self, quantity):
+        return quantity * self.L_cactus
+    
+    def convert_to_cactus_time(self, quantity):
+        return quantity / self.T_cactus
+    
+    def convert_from_cactus_time(self, quantity):
+        return quantity * self.T_cactus
+    
+    def convert_to_cactus_rho(self, quantity):
+        return quantity * self.L_cactus ** 3 / self.solar_mass
+    
+    def convert_from_cactus_rho(self, quantity):
+        return quantity * self.solar_mass / self.L_cactus ** 3
+    
+    def convert_to_cactus_pressure(self, quantity):
+        return self.convert_to_cactus_rho(quantity) * self.speed_light ** 2
+    
+    def convert_from_cactus_pressure(self, quantity):
+        return self.convert_from_cactus_rho(quantity) / self.speed_light ** 2
+    
+    def convert_to_cactus_velocity(self, quantity):
+        return quantity / self.speed_light
+    
+    def convert_from_cactus_velocity(self, quantity):
+        return quantity * self.speed_light
