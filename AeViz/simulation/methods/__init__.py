af8df8c8692cb956b506e705efb27a9b6f2b5e6d
Marco Cusinato <marco.cusinato@uv.es>
2025-03-05 10:42:48 +0100

    Converted spherichal harmonics to aerray

--- AeViz/simulation/methods/__init__.py
+++ AeViz/simulation/methods/__init__.py
@@ -1,5 +1,5 @@
 from AeViz.units import u
-from AeViz.units.aerray import aerray
+from AeViz.units.aeseries import aerray, aeseries
 from AeViz.utils.decorators import (smooth, derive, hdf_isopen,
                                     subtract_tob, sum_tob)
 import numpy as np
--- AeViz/simulation/methods/sph_harmonics.py
+++ AeViz/simulation/methods/sph_harmonics.py
@@ -20,8 +20,16 @@ def rho_spherical_harmonics(self, l=0, m=None, zero_norm=True,
                             save_checkpoints=True, **kwargs):
     if m is None:
         calculate_rho_decomposition(self, save_checkpoints, msum=True)
+        if zero_norm:
+            lb = r'$\tilde{\rho}_{%d}/\tilde{\rho}_{00}$' % l
+        else:
+            lb = r'$\tilde{\rho}_{%d}$' % l
     else:
         calculate_rho_decomposition(self, save_checkpoints)
+        if zero_norm:
+            lb = r'$\tilde{\rho}_{%d,%d}/\tilde{\rho}_{00}$' % (l, m)
+        else:
+            lb = r'$\tilde{\rho}_{%d,%d}$' % (l, m)
     if [rhomin, rhomax, r] == [None, None, None]:
         radius = self.cell.radius(self.ghost)
         time, rlm = get_sph_profile(self, l, m)
@@ -31,10 +39,23 @@ def rho_spherical_harmonics(self, l=0, m=None, zero_norm=True,
             else:
                 _, r00 = get_sph_profile(self, 0, 0)
             rlm /= r00
-        return time, radius, rlm
+        time = aerray(time, u.s, name='time', label=r'$t-t_\mathrm{b}$',
+                      cmap=None, log=False,
+                 limits=[-0.005, time.max()])
+        rlm = aerray(rlm, u.dimensionless_unscaled, 'rho_sph_harmonics',
+                     lb, 'YlGn',
+                     [rlm.min() * 1.1, rlm.max() * 0.9], False)
+        return aeseries(rlm, time=time, radius=radius)
     else:
-        return get_sph_profiles_r(self, l=l, m=m, rhomin=rhomin,
+        time, rlm = get_sph_profiles_r(self, l=l, m=m, rhomin=rhomin,
                                     rhomax=rhomax, r=r, zero_norm=zero_norm)
+        time = aerray(time, u.s, name='time', label=r'$t-t_\mathrm{b}$',
+                      cmap=None, log=False,
+                 limits=[-0.005, time.max()])
+        rlm = aerray(rlm, u.dimensionless_unscaled, 'rho_sph_harmonics',
+                     lb, 'YlGn',
+                     [rlm.min * 1.1, rlm.max() * 0.9], False)
+        return aeseries(rlm, time=time)
 
 def data_for_barcode(self, lmin=None, lmax=None, msum=False,
                         r=None, rhomin=None, rhomax=None, save_checkpoints=True,
@@ -44,6 +65,24 @@ def data_for_barcode(self, lmin=None, lmax=None, msum=False,
     harmonics decomposition of the density.
     """
     calculate_rho_decomposition(self, save_checkpoints, msum=True)
-    return get_data_for_barcode(self, lmax=lmax, lmin=lmin, msum=msum,
+    time, Y, data = get_data_for_barcode(self, lmax=lmax, lmin=lmin, msum=msum,
                                 r=r, rhomin=rhomin, rhomax=rhomax,
                                 zero_norm=zero_norm)
+    time = aerray(time, u.s, name='time', label=r'$t-t_\mathrm{b}$',
+                  cmap=None, log=False,
+             limits=[-0.005, time.max()])
+    if msum:
+        Y = aerray(Y, u.dimensionless_unscaled, name='l', label=r'$l$',
+                   cmap=None, log=False, limits=[Y.min(), Y.max()])
+    else:
+        Y = aerray(Y, u.dimensionless_unscaled, name='lm', label=r'$m$',
+                   cmap=None, log=False, limits=[Y.min(), Y.max()])
+    if zero_norm:
+        data = aerray(data, u.dimensionless_unscaled, name='rho_sph_harmonics',
+                  label=r'$\tilde{\rho}/\tilde{\rho}_{00}$', cmap='seismic',
+                  log=False, limits=[data.min() * 1.1, data.max() * 0.9])
+    else:
+        data = aerray(data, u.dimensionless_unscaled, name='rho_sph_harmonics',
+                  label=r'$\tilde{\rho}$', cmap='seismic',
+                  log=False, limits=[data.min() * 1.1, data.max() * 0.9])
+    return aeseries(data, time=time, Y=Y)
