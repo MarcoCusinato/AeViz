Corrected SNR computation and added windowing renormalization

AeViz/units/aeseries.py
@@ -474,13 +474,15 @@ class aeseries:
                                 np.argmax(self.time >= time_range[1]), 1)
         if windowing is None:
             window = 1.
+            wind_norm = 1.
         else:
             window = getattr(np, windowing)(indices[1] - indices[0])
+            wind_norm = np.sum(window) / len(window)
         dt = (self.time[indices][-1] - self.time[indices][0]).value
         freq = np.fft.fftfreq(len(self.time[indices]),
                               np.mean(np.diff(self.time[indices].to(u.s).value)))
         tilde_signal = np.abs(np.fft.fft(self.data[indices].value * window,
-                                         norm=norm, axis=axis)) * dt
+                                         norm=norm, axis=axis) / wind_norm) * dt
         if norm_by_max:
             tilde_signal /= np.abs(tilde_signal).max()
             units = u.dimensionless_unscaled
@@ -536,12 +538,14 @@ class aeseries:
         dt = (self.time[indices][-1] - self.time[indices][0]).value
         if windowing is None:
             window = 1.
+            wind_norm = 1.
         else:
             window = getattr(np, windowing)(len(self.time[indices]))
+            wind_norm = np.sum(window) / len(window)
         freq = np.fft.rfftfreq(len(self.time[indices]),
                               np.mean(np.diff(self.time[indices].to(u.s).value)))
         tilde_signal = np.abs(np.fft.rfft(self.data[indices].value * window,
-                                          norm=norm, axis=axis)) * dt
+                                          norm=norm, axis=axis) / wind_norm) * dt
         if norm_by_max:
             tilde_signal /= np.abs(tilde_signal).max()
             units = u.dimensionless_unscaled

AeViz/utils/physics/GW_utils.py
@@ -1489,7 +1489,7 @@ def compute_SNR(simulation, detector, comp, distance=(u.kpc *10),
     ## Now we proceed with the actual computation of the SNR
     ## We assume constant spacing in the frequency domain
     df = np.mean(np.diff(hchar.frequency))
-    SNR = 4 * df * hchar.data ** 2 / (ASD.data ** 2 * ASD.frequency ** 2)
+    SNR = df * hchar.data ** 2 / (ASD.data ** 2 * ASD.frequency ** 2)
     ##Consistency check
     if SNR.unit != u.dimensionless_unscaled:
         raise ValueError("Units mismatch")